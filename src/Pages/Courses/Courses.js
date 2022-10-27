864fe1e17f0a02f6af4fadef848ddcd18b8a6a12
farhadbgd <farhadbgd@outlook.com>
2022-10-27 19:57:03 +0600

    pdf download create

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -3,20 +3,35 @@ import { useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import jsPDF from 'jspdf'
 
 const Courses = () => {
     const courses = useLoaderData();
     console.log(courses[0])
+    const generatePDF = () => {
+        var doc = new jsPDF('p', 'pt');
+        doc.addFont('helvetica', 'normal')
+        doc.text(20, 20, `Name of the Certificate is ${courses[0].title}`)
+        doc.text(20, 40, `Duration is in Hours ${courses[0].duration}`)
+        doc.text(20, 60, `Cost in USD is ${courses[0].fee}`)
+        doc.text(20, 80, `Trainer Name is ${courses[0].trainer}`)
+        console.log(courses)
+        doc.save('demo.pdf')
+    }
+
     return (
         <div>
-            <h1>This is Courses Page: {courses.length}</h1>
+            <div>
+                <h3>Course Information || ID NO: {courses[0]._id}</h3>
+                <button onClick={generatePDF} type="primary">Download PDF</button>
+            </div>
+
             <Card >
-                <Card.Img variant="top" src={courses[0].image_url} />
+                <Card.Img variant="top" src={courses[0].img} />
                 <Card.Body>
                     <Card.Title><Link style={{ textDecoration: 'none' }} >{courses[0].details}</Link></Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
+
                     </Card.Text>
 
                 </Card.Body>
